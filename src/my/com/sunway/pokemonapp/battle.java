c0c195ad103a49515e0b1cacf3f565c43317dfe9
sygoh0909 <shuyigoh2004@gmail.com>
2024-06-19 18:23:14 +0800

    testing

--- src/my/com/sunway/pokemonapp/battle.java
+++ src/my/com/sunway/pokemonapp/battle.java
@@ -1,7 +1,8 @@
-package battle;
+package my.com.sunway.pokemonapp;
 
 public class battle {
 	public static void main(String[] args) {
+		System.out.println("testing");
 
 	}
 
---
+++ src/my/com/sunway/pokemonapp/game.java
@@ -0,0 +1,5 @@
+package my.com.sunway.pokemonapp;
+
+public class game {
+
+}
